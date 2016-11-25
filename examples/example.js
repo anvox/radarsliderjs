dc3973ff2ac5664efb646ac45ac86099a44472ba
An Vo <thien.an.vo.nguyen@gmail.com>
2016-11-24 18:22:03 -0800

    Update example

---
+++ examples/example.js
@@ -0,0 +1,20 @@
+var RadarSlider = require('../lib/index');
+RadarSlider.init(0,5,1,
+  [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Nullam vehicula tincidunt sapien, eget ullamcorper urna luctus quis.",
+    "Proin tempor lacus turpis. ",
+    "Praesent.",
+    "Donec vitae ullamcorper nisi, eu varius mi.",
+    "Aenean tincidunt dictum odio, eu lacinia mauris sollicitudin sit amet.",
+    "Curabitur pellentesque pellentesque arcu"],
+  [0,3,2,4,2,1,1]);
+// var data = new RadarSlider.SliderModel(0,5,1,
+//   [ "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+//     "Nullam vehicula tincidunt sapien, eget ullamcorper urna luctus quis.",
+//     "Proin tempor lacus turpis. ",
+//     "Praesent.",
+//     "Donec vitae ullamcorper nisi, eu varius mi.",
+//     "Aenean tincidunt dictum odio, eu lacinia mauris sollicitudin sit amet.",
+//     "Curabitur pellentesque pellentesque arcu"],
+//   [0,3,2,4,2,1,1]);
+// var controller = new RadarSlider.RadarController(data);
